a5cfe63023ea26e338efce868a239a18f58dc74a
Leonardo Jacon <leonardojacon2013@gmail.com>
2026-09-03 17:14:02 -0300

    arrumando erro e colocando a secret key no compose

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
 import { generateToken } from "../utils/jwt";
+import { AppError } from "../errors/AppError";
 
 export class AuthService {
 constructor(private prisma: PrismaClient) {}
@@ -8,7 +9,7 @@ constructor(private prisma: PrismaClient) {}
 async register(email:string, senha:string, nome:string){
 const existingUser = await this.prisma.usuario.findUnique({where:{email}});
 if (existingUser) {
-  throw new Error("Email já está em uso!");
+  throw new AppError("Email já está em uso!", 409);
 }
 const hashedPassword = await bcrypt.hash(senha,10);
 const user = await this.prisma.usuario.create({
@@ -23,11 +24,11 @@ const user = await this.prisma.usuario.create({
     async login(email:string, senha:string){
         const user = await this.prisma.usuario.findUnique({where:{email}});
         if (!user) {
-            throw new Error("Usuário não encontrado! (Email e/ou senha inválidos.)");
+          throw new AppError("Email e/ou senha inválidos.", 401);
         }
         const passwordCorrect = await bcrypt.compare(senha, user.senha);
         if (!passwordCorrect) {
-            throw new Error("Senha incorreta! (Email e/ou senha inválidos.)");
+          throw new AppError("Email e/ou senha inválidos.", 401);
         }
         const token = generateToken(user.id);
         return {token};
