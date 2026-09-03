594e782c8ade711b16f7f6d3b966115c6f66b69e
Leonardo Jacon <leonardojacon2013@gmail.com>
2026-09-02 22:56:11 -0300

    Adicionando pino para resposta à requisições HTTPS get e post e adicionando segurança na log https

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,19 @@ import cors from "cors";
 import { createRoutes } from "./routes";
 import { errorHandler } from "./middleware/errorHandler";
 import { container } from "./container";
+import { rateLimiter } from "./middleware/rateLimiter";
+import { httpLogger } from "./logger";
+
 
 export function createApp() {
   const app = express();
 
   app.use(cors()); 
   app.use(express.json()); 
+
+  app.use(httpLogger);
+
+  app.use(rateLimiter);
 
   app.use(createRoutes(container));
 
